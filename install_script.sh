ab8163ef82e1069a29a44b8976e3c5c90ae270eb
superfrost <otkycu@gmail.com>
2020-11-04 13:15:55 +0300

    fix install script echo

--- install_script.sh
+++ install_script.sh
@@ -10,9 +10,10 @@ npm install
 echo "Creating database"
 sqlite3 my_db.db < create_db.sql
 
-echo "Creating Telegram Token"
-read -p 'Enter your Telegram Token: ' telegram_token
+echo "Creating .env file for Telegram Token"
 echo "TELEGRAM_TOKEN=$telegram_token" > .env
+echo -e "\033[0;33mDon't forget add telegran token to \033[1;35m.env\033[0;33m file!!!\033[m"
+echo -e "Type command: \033[1;35mnano $HOME/telegram_expenses_bot/.env\033[m"
 
 # if you have installed pm2
 # pm2 start beter_bot.js
